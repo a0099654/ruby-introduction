0cf829c56193f16f6b7ac9099e9b6e71da0141ba
a0099654 <a0099654@u.nus.edu>
2016-11-20 12:04:00 +0800

    [ADD]Math operation

---
+++ document.rb
@@ -0,0 +1,8 @@
+word = <<document
+WOW, IS THIS THE document
+AND THAT'S WHAT we need
+    123, "hahaha"
+    what can i do for THIS
+document
+
+print word
---
+++ math.rb
@@ -0,0 +1,4 @@
+puts -23.abs
+puts 12.div(2)
+puts 12.modulo(7)
+puts 100.to_s
